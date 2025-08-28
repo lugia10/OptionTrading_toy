d6591f5085b6cb9555838d800fed770608dac411
kolee94 <kolee94@kaist.ac.kr>
2025-08-28 18:02:14 +0900

    Add main application logic and XASession class for server connection
    - Implemented the Main class to initialize the application and connect to the server.
    - Added the XASession class to manage server connections, including server selection based on login type.
    - Integrated win32com for COM object handling in the XASession class.

--- source/core/xa_session.py
+++ source/core/xa_session.py
@@ -1,8 +1,24 @@
+import win32com.client
+import pythoncom
+import sys
+
 class XASessionReceiver:
     def __init__(self):
-        pass
+        self.parent = None
 
 class XASession:
-    def __init__(self):
-        pass
+    def __init__(self, login_server):
+        self.login_server = self.set_server(login_server=login_server)
+        self.session = win32com.client.DispatchWithEvents("XA_Session.XASession", XASessionReceiver)
 
+    @staticmethod
+    def set_server(login_server):
+        if login_server == "실투자":
+            return "hts.ebestsec.co.kr"
+        elif login_server == "모의투자":
+            return "demo.ebestsec.co.kr"
+
+    def connect_server(self):
+        res = self.session.ConnectServer(self.login_server, 20001)
+        print(res)
+        
